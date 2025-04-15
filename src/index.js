04eff3c82f719023a6cfe9a7290539720bbf9286
Firas <firaselhaj3@gmail.com>
2025-04-15 20:22:26 +0100

    adding cron.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import authRoutes from "./routes/authRoutes.js";
 import postRoute from "./routes/postRoute.js";
 import {connectDB} from "./lib/db.js";
 import cors from "cors";
-
+import job from "./lib/cron.js"
 const app = express(); 
 const PORT=process.env.PORT||3000 ; 
+job.start(); 
 app.use(express.json()); 
 app.use(cors()); 
 app.use("/api/auth",authRoutes)
