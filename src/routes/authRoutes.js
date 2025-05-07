ab6cf01472fd92e9e3d505fa3b74eaf74620430a
Firas <firaselhaj3@gmail.com>
2025-05-07 23:07:33 +0100

    reset password link

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -162,7 +162,7 @@ router.post("/forgot-password", async (req, res) => {
 
     await user.save();
 
-    const resetUrl = `${process.env.API_URL}/reset-password?token=${resetToken}`;
+    const resetUrl = `finderapp://reset-password/${resetToken}`;
 
     // Send email
     const mailOptions = {
